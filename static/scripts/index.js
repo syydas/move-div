442f5056e5e99a2681649cc48de8d6588f718cd0
Asuu <suqinzheng_0624@163.com>
2019-12-19 21:47:30 +0800

    [zhengsuqin] feat: add a function to stop the green block when mouse up

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -7,33 +7,40 @@ window.onload = function() {
         var initialY = e.clientY - this.offsetTop;
 
         mouseMove(initialX, initialY);
-        //mouseUp();
+        mouseUp(initialX, initialY);
     }
 
 
-    function mouseMove(x, y) {
+    function mouseMove(left, top) {
         document.onmousemove = function(e) {
-            var moveX = e.clientX - x;
-            var moveY = e.clientY - y;
+            var moveX = e.clientX - left;
+            var moveY = e.clientY - top;
 
             movelimit(moveX, moveY);
         }
     }
 
-    function movelimit(x, y) {
-        if ((x > 0 && x <= 920) && (y > 0 && y <= 720)) {
-            moveBlock.style.left = x + "px";
-            moveBlock.style.top = y + "px";
+    function mouseUp(left, top) {
+        document.onmouseup = function(e) {
+            document.onmousemove = null;
+            document.onmousedown = null;
+        }
+    }
+
+    function movelimit(left, top) {
+        if ((left > 0 && left <= 920) && (top > 0 && top <= 720)) {
+            moveBlock.style.left = left + "px";
+            moveBlock.style.top = top + "px";
         } else {
             moveBlock.style.left = (function() {
-                if (x < 0) {
+                if (left < 0) {
                     return 0 + "px";
                 } else {
                     return 920 + "px";
                 }
             });
             moveBlock.style.top = (function() {
-                if (y < 0) {
+                if (top < 0) {
                     return 0 + "px";
                 } else {
                     return 720 + "px";
