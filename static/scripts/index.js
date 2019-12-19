c762c487e696e2a08c8144013c3ddc327c0d166a
Asuu <suqinzheng_0624@163.com>
2019-12-19 21:30:55 +0800

    [zhengsuqin] feat: add a function to move the green block and to limit the move area

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -3,11 +3,42 @@ window.onload = function() {
     var staticBlock = document.getElementById("static-block");
 
     moveBlock.onmousedown = function(e) {
-        e = e || event;
         var initialX = e.clientX - this.offsetLeft;
         var initialY = e.clientY - this.offsetTop;
 
-        //mouseMove();
+        mouseMove(initialX, initialY);
         //mouseUp();
+    }
+
+
+    function mouseMove(x, y) {
+        document.onmousemove = function(e) {
+            var moveX = e.clientX - x;
+            var moveY = e.clientY - y;
+
+            movelimit(moveX, moveY);
+        }
+    }
+
+    function movelimit(x, y) {
+        if ((x > 0 && x <= 920) && (y > 0 && y <= 720)) {
+            moveBlock.style.left = x + "px";
+            moveBlock.style.top = y + "px";
+        } else {
+            moveBlock.style.left = (function() {
+                if (x < 0) {
+                    return 0 + "px";
+                } else {
+                    return 920 + "px";
+                }
+            });
+            moveBlock.style.top = (function() {
+                if (y < 0) {
+                    return 0 + "px";
+                } else {
+                    return 720 + "px";
+                }
+            });
+        }
     }
 }
